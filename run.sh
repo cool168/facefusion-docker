a9ce68b270fe668e2d05a8d5fbc346a47ccf34fe
cool168 <cool168@users.noreply.github.com>
2024-12-30 12:13:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export GRADIO_SERVER_NAME=0.0.0.0
 export PIP_BREAK_SYSTEM_PACKAGES=1
-CUDA_VISIBLE_DEVICES=0 python facefusion.py run
+CUDA_VISIBLE_DEVICES=1 python facefusion.py run
 
